df4a977fed949b028848fdfe149215ff4ad6a880
firasmindhi123 <109851214+firasmindhi123@users.noreply.github.com>
2024-08-27 12:25:29 +0530

    authenticate

--- router/exercise.js
+++ router/exercise.js
@@ -1,6 +1,7 @@
 const express =require('express')
 const router =express.Router()
 const exerciseController=require('../controller/exerciseController')
+const authentication= require('../autentication')
 router.get('/exerciseDetail/:id',exerciseController.getExercise)
 router.get('/detail',exerciseController.getexercises)
 router.post('/update/:id',authentication.authenticate,exerciseController.updateExercise)
